10cdb6bd37ca27fe0ceb86c5da030bf154931bb7
solsol <021014sarah@gmail.com>
2022-07-12 01:35:41 +0900

    gitignore test

--- ffmpeg명령어/test.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system('ffmpeg -headers "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36" -i "https://lmsvod.konkuk.ac.kr:1936/vod/mp4:wowza_vod/22940/405693/900761_720p.mp4/playlist.m3u8" -c copy -bsf:a aac_adtstoasc "E:\학교\C프\강의영상\C-3-1.mp4"')
