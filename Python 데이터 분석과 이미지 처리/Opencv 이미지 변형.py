b5f0c2eb1eace4106cd1af1ab85f92c3e607e323
Solsol <k202211360@konkuk.ac.kr>
2022-07-18 16:42:12 +0900

    opencv 이미지 변경 하다맘 ㅋㅎ

---
+++ Python 데이터 분석과 이미지 처리/Opencv 이미지 변형.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+from numpy import imag
+
+image=cv.imread('Python 데이터 분석과 이미지 처리/Cat03.jpg')
+cv.imshow('image', image)
+cv.waitKey(0)
+
+expand=cv.resize(image, None, fx=1.5, fy=1.5, interpolation=cv.INTER_CUBIC)
+cv.imshow('Image', expand)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+shrink=cv.resize(image, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
+cv.imshow('Shrink', shrink)
+cv.waitKey(0)
+
+shrinkexpand=cv.resize(shrink, None, fx=2, fy=2, interpolation=cv.INTER_AREA)
+cv.imshow('shrinkexpand', shrinkexpand)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+for i in range(500):
+    shrink=cv.resize(shrinkexpand, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
+    shrinkexpand=cv.resize(shrink, None, fx=2, fy=2, interpolation=cv.INTER_AREA)
+
+cv.imshow('shrinkexpand', shrinkexpand)
+cv.waitKey(0)
+cv.imshow('original', image)
+cv.waitKey(0)
