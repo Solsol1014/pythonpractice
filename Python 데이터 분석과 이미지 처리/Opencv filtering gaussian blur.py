f4663ecbee1cf852aa06e8c895e6721d869c862b
solsol <021014sarah@gmail.com>
2022-07-28 01:06:47 +0900

    오타 수정!

--- Python 데이터 분석과 이미지 처리/Opencv filtering gaussian blur.py
+++ Python 데이터 분석과 이미지 처리/Opencv filtering gaussian blur.py
@@ -4,7 +4,7 @@ image=cv.imread('gray_image.jpg')
 cv.imshow('image', image)
 cv.waitKey(0)
 
-#kernel size는 혼수여야함
+#kernel size는 홀수여야함
 dst=cv.GaussianBlur(image, (5, 5), 0)
 cv.imshow('image', dst)
 cv.waitKey(0)
